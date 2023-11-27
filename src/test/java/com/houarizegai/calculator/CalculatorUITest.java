829c9ac0bc07703850afce80047c23fe9923e632
Sunil Agarwal <130535921+sunil-singularityx@users.noreply.github.com>
2023-11-27 17:35:24 +0530

    Update CalculatorUITest.java

--- src/test/java/com/houarizegai/calculator/CalculatorUITest.java
+++ src/test/java/com/houarizegai/calculator/CalculatorUITest.java
@@ -14,11 +14,13 @@ class CalculatorUITest {
     @BeforeEach
     void setUp() {
         calculatorUI = new CalculatorUI();
+        
     }
 
     @ParameterizedTest
     @CsvSource({"3,5,+,8", "2,8,-,-6", "44.5,10,*,445", "320,5,/,64", "3,5,%,3", "5,3,^,125"})
     void testCalculation(double firstNumber, double secondNumber, char operator, double expectedResult) {
+        
         assertEquals(expectedResult, calculatorUI.calculate(firstNumber, secondNumber, operator));
     }
 }
