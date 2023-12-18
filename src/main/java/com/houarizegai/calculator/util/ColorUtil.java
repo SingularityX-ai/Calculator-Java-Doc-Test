337d77580e116c74e6acb201f9f37e6a017e9cbf
Suman Saurabh <ss.sumansaurabh92@gmail.com>
2023-12-19 01:33:38 +0530

    Update ColorUtil.java

--- src/main/java/com/houarizegai/calculator/util/ColorUtil.java
+++ src/main/java/com/houarizegai/calculator/util/ColorUtil.java
@@ -28,4 +28,14 @@ public class ColorUtil {
                 .orElse(null);
         
     }
+
+    public static Color hex2Color(String colorHex, String dType) {
+        return Optional.ofNullable(colorHex )
+                .map(hex -> new Color (
+                        Integer.valueOf( colorHex.substring(0, 2), 16),
+                        Integer.valueOf(colorHex.substring(2, 4), 16),
+                        Integer.valueOf(colorHex.substring(4, 6), 16)))
+                .orElse(null);
+        
+    }
 }
